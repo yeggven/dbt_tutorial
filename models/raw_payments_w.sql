002713162b2d861835d416ec762d1aaa1cc711fc
Yevgen <yevgen@Yevgens-MacBook-Pro.local>
2022-03-30 22:20:06 +0200

    commet

--- models/raw_payments_w.sql
+++ models/raw_payments_w.sql
@@ -1,9 +1,11 @@
+{% set payment_methods = ["bank_transfer", "credit_card", "gift_card"] %}
+
 select
 order_id,
 'test' as test,
 {% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
 sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
-{% endfor %}
+{% if not loop.last %},{% endif %}
 sum(amount) as total_amount
 from {{ ref('raw_payments') }}
 group by 1
