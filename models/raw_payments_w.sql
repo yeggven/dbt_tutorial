3bd0c269ba1098cf4abc105b44e359828c1871e4
Yevgen <yevgen@Yevgens-MacBook-Pro.local>
2022-03-30 14:08:24 +0200

    commet

--- models/raw_payments_w.sql
+++ models/raw_payments_w.sql
@@ -1,7 +1,7 @@
 select
 order_id,
 'test' as test,
-{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}.
+{% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %},
 sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
 {% endfor %},
 sum(amount) as total_amount
