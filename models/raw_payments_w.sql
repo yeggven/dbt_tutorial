03978e9529eaf083cf9ea52c9f0b8a003e08e7da
Yevgen <yevgen@Yevgens-MacBook-Pro.local>
2022-03-30 22:11:42 +0200

    mm

--- models/raw_payments_w.sql
+++ models/raw_payments_w.sql
@@ -1,6 +1,6 @@
 select
 order_id,
-'test' as test
+'test' as test,
 {% for payment_method in ["bank_transfer", "credit_card", "gift_card"] %}
 sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
 {% endfor %}
